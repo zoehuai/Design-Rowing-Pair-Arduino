7e356ddbc82f929a6aa46ca8f07c18a674eb8935
Zoe Huai <54278902+zoehuai@users.noreply.github.com>
2024-01-30 05:52:49 +1100

    Update Machine.ino

--- Machine.ino
+++ Machine.ino
@@ -58,7 +58,6 @@ void loop() {
   
     Serial.println(speed);}
   distance1 = distance2;
-  /*
   if(speed > 3 ){
    for (pos = 0; pos <= 180; pos += 30) { // goes from 0 degrees to degrees
 // in steps of 1 degree
