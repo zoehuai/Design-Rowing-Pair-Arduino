1e795dde4aaeb3107408c8149e2879cb8e88107e
Zoe Huai <54278902+zoehuai@users.noreply.github.com>
2024-01-30 05:51:42 +1100

    Update and rename newest_and_workable.ino to Machine.ino

--- Machine.ino
+++ Machine.ino
@@ -1,4 +1,4 @@
-/*
+
 #include <Servo.h>
 int trigPin=10;
 int echoPin=11;
@@ -106,4 +106,4 @@ myservo1.write(pos);
 for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
 myservo1.write(pos);  
 }
- */
+ 
